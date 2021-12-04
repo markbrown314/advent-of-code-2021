d3e6d6220850e0cefde1ab009382a11f478e6262
Mark F. Brown <mark.brown314@gmail.com>
2021-12-04 06:28:06 -0500

    Cleaned up Part 1

--- day-3/day-3.go
+++ day-3/day-3.go
@@ -4,52 +4,66 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
-func main() {
+func convertInput(input []byte) ([]uint32, int) {
+	binArray := make([]uint32, 0, 1000)
+	lines := strings.Split(string(input), "\n")
+	binLength := 0
+	for _, valueStr := range lines {
+		valueInt, err := strconv.ParseUint(valueStr, 2, 32)
+		if err != nil {
+			log.Fatalf("error during string conversion %v\n", err)
+		}
+		if len(valueStr) > binLength {
+			binLength = len(valueStr)
+		}
+		binArray = append(binArray, uint32(valueInt))
+	}
+	return binArray, binLength
+}
 
+func countBits(binArray []uint32, binLength int, pos int) int {
+	one_count := 0
+	zero_count := 0
+	for j := 0; j < len(binArray); j++ {
+		switch (binArray[j] >> (binLength - pos - 1)) & 1 {
+		case 0:
+			zero_count++
+		case 1:
+			one_count++
+		}
+	}
+
+	if zero_count > one_count {
+		return 0
+	}
+
+	return 1
+}
+
+func part1(binArray []uint32, binLength int) {
+
+	var gammaRate uint32 = 0
+	var epsilonRate uint32 = 0
+
+	for i := 0; i < binLength; i++ {
+		bitSetting := countBits(binArray, binLength, i)
+		gammaRate = gammaRate<<1 | uint32(bitSetting)
+		epsilonRate = epsilonRate<<1 | uint32(^bitSetting&0x1)
+	}
+
+	fmt.Printf("part 1: power consumption %v\n", gammaRate*epsilonRate)
+
+}
+
+func main() {
 	input, err := ioutil.ReadFile("day-3-input-1.txt")
 	if err != nil {
 		log.Fatalf("error reading input file %v", err)
 	}
-
-	lines := strings.Split(string(input), "\n")
-
-	var gamma_rate uint32 = 0
-	var epsilon_rate uint32 = 0
-
-	for i := 0; i < len(lines[i]); i++ {
-		one_count := 0
-		zero_count := 0
-		for j := 0; j < len(lines); j++ {
-			switch lines[j][i] {
-			case '0':
-				zero_count++
-			case '1':
-				one_count++
-			}
-		}
-
-		var bit_setting uint32 = 1
-		if zero_count > one_count {
-			bit_setting = 0
-		}
-
-		if gamma_rate == 0 {
-			gamma_rate |= bit_setting
-		} else {
-			gamma_rate = gamma_rate<<1 | bit_setting
-		}
-
-		if epsilon_rate == 0 {
-			epsilon_rate |= (^bit_setting & 0x1)
-		} else {
-			epsilon_rate = epsilon_rate<<1 | (^bit_setting & 0x1)
-		}
-
-		//fmt.Printf("pos %v zero_count %v one_count %v gamma_rate %b epsilon_rate %b\n", i, zero_count, one_count, gamma_rate, epsilon_rate)
-	}
-
-	fmt.Printf("part 1: power consumption %v\n", gamma_rate*epsilon_rate)
+	binArray, binLength := convertInput(input)
+	part1(binArray, binLength)
 }
