e0237ba374958b1ebddc7f1ab7ba0d59fe3833e7
Mark F. Brown <mark.brown314@gmail.com>
2021-12-04 07:41:04 -0500

    Solved Day 3

--- day-3/day-3.go
+++ day-3/day-3.go
@@ -25,38 +25,80 @@ func convertInput(input []byte) ([]uint32, int) {
 	return binArray, binLength
 }
 
-func countBits(binArray []uint32, binLength int, pos int) int {
-	one_count := 0
-	zero_count := 0
+func countBits(binArray []uint32, binLength int, pos int) ([]uint32, []uint32) {
+	binArrayZeroes := make([]uint32, 0, len(binArray))
+	binArrayOnes := make([]uint32, 0, len(binArray))
+
 	for j := 0; j < len(binArray); j++ {
 		switch (binArray[j] >> (binLength - pos - 1)) & 1 {
 		case 0:
-			zero_count++
+			binArrayZeroes = append(binArrayZeroes, binArray[j])
 		case 1:
-			one_count++
+			binArrayOnes = append(binArrayOnes, binArray[j])
 		}
 	}
 
-	if zero_count > one_count {
-		return 0
-	}
-
-	return 1
+	return binArrayZeroes, binArrayOnes
 }
 
 func part1(binArray []uint32, binLength int) {
-
 	var gammaRate uint32 = 0
 	var epsilonRate uint32 = 0
 
 	for i := 0; i < binLength; i++ {
-		bitSetting := countBits(binArray, binLength, i)
+		bitSetting := 0
+		binArrayZeroes, binArrayOnes := countBits(binArray, binLength, i)
+		if len(binArrayOnes) > len(binArrayZeroes) {
+			bitSetting = 1
+		}
 		gammaRate = gammaRate<<1 | uint32(bitSetting)
 		epsilonRate = epsilonRate<<1 | uint32(^bitSetting&0x1)
 	}
 
 	fmt.Printf("part 1: power consumption %v\n", gammaRate*epsilonRate)
 
+}
+
+func part2(binArray []uint32, binLength int) {
+	o2BinArray := make([]uint32, len(binArray))
+	cO2BinArray := make([]uint32, len(binArray))
+
+	copy(o2BinArray, binArray)
+	copy(cO2BinArray, binArray)
+
+	for i := 0; i < binLength; i++ {
+		if len(o2BinArray) == 1 {
+			break
+		}
+		binArrayZeroes, binArrayOnes := countBits(o2BinArray, binLength, i)
+		if len(binArrayOnes) >= len(binArrayZeroes) {
+			o2BinArray = make([]uint32, len(binArrayOnes))
+			copy(o2BinArray, binArrayOnes)
+		} else {
+			o2BinArray = make([]uint32, len(binArrayZeroes))
+			copy(o2BinArray, binArrayZeroes)
+		}
+	}
+
+	o2ScrubberRating := o2BinArray[0]
+
+	for i := 0; i < binLength; i++ {
+		if len(cO2BinArray) == 1 {
+			break
+		}
+		binArrayZeroes, binArrayOnes := countBits(cO2BinArray, binLength, i)
+		if len(binArrayZeroes) <= len(binArrayOnes) {
+			cO2BinArray = make([]uint32, len(binArrayZeroes))
+			copy(cO2BinArray, binArrayZeroes)
+		} else {
+			cO2BinArray = make([]uint32, len(binArrayOnes))
+			copy(cO2BinArray, binArrayOnes)
+		}
+	}
+
+	cO2ScrubberRating := cO2BinArray[0]
+
+	fmt.Println("part 2: life support rating:", o2ScrubberRating*cO2ScrubberRating)
 }
 
 func main() {
@@ -66,4 +108,5 @@ func main() {
 	}
 	binArray, binLength := convertInput(input)
 	part1(binArray, binLength)
+	part2(binArray, binLength)
 }
