847b02c8d6551051208a1edbd540b3a2881263a8
Mark F. Brown <mark.brown314@gmail.com>
2021-12-04 04:03:11 -0500

    Part 1 Solved

--- day-3/day-3.go
+++ day-3/day-3.go
@@ -9,15 +9,16 @@ import (
 
 func main() {
 
-	input, err := ioutil.ReadFile("day-3-test.txt")
+	input, err := ioutil.ReadFile("day-3-input-1.txt")
 	if err != nil {
 		log.Fatalf("error reading input file %v", err)
 	}
 
 	lines := strings.Split(string(input), "\n")
-	fmt.Println(len(lines))
 
 	var gamma_rate uint32 = 0
+	var epsilon_rate uint32 = 0
+
 	for i := 0; i < len(lines[i]); i++ {
 		one_count := 0
 		zero_count := 0
@@ -29,6 +30,7 @@ func main() {
 				one_count++
 			}
 		}
+
 		var bit_setting uint32 = 1
 		if zero_count > one_count {
 			bit_setting = 0
@@ -40,6 +42,14 @@ func main() {
 			gamma_rate = gamma_rate<<1 | bit_setting
 		}
 
-		fmt.Printf("pos %v zero_count %v one_count %v gamma_rate %b epsilon_rate %b\n", i, zero_count, one_count, gamma_rate, ^gamma_rate)
+		if epsilon_rate == 0 {
+			epsilon_rate |= (^bit_setting & 0x1)
+		} else {
+			epsilon_rate = epsilon_rate<<1 | (^bit_setting & 0x1)
+		}
+
+		//fmt.Printf("pos %v zero_count %v one_count %v gamma_rate %b epsilon_rate %b\n", i, zero_count, one_count, gamma_rate, epsilon_rate)
 	}
+
+	fmt.Printf("part 1: power consumption %v\n", gamma_rate*epsilon_rate)
 }
