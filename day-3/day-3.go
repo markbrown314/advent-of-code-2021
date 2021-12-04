5e280dddc9cf72167cf0db7c4565cfeb462797bf
Mark F. Brown <mark.brown314@gmail.com>
2021-12-03 22:05:19 -0500

    Continuing Day 3

--- day-3/day-3.go
+++ day-3/day-3.go
@@ -17,4 +17,29 @@ func main() {
 	lines := strings.Split(string(input), "\n")
 	fmt.Println(len(lines))
 
+	var gamma_rate uint32 = 0
+	for i := 0; i < len(lines[i]); i++ {
+		one_count := 0
+		zero_count := 0
+		for j := 0; j < len(lines); j++ {
+			switch lines[j][i] {
+			case '0':
+				zero_count++
+			case '1':
+				one_count++
+			}
+		}
+		var bit_setting uint32 = 1
+		if zero_count > one_count {
+			bit_setting = 0
+		}
+
+		if gamma_rate == 0 {
+			gamma_rate |= bit_setting
+		} else {
+			gamma_rate = gamma_rate<<1 | bit_setting
+		}
+
+		fmt.Printf("pos %v zero_count %v one_count %v gamma_rate %b epsilon_rate %b\n", i, zero_count, one_count, gamma_rate, ^gamma_rate)
+	}
 }
