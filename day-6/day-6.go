89cbd585ecbe8106e0631aa643b6ba09d2968fd6
Mark F. Brown <mark.brown314@gmail.com>
2021-12-18 21:03:45 -0500

    Solved Day 6 Part 2

--- day-6/day-6.go
+++ day-6/day-6.go
@@ -8,41 +8,55 @@ import (
 	"strings"
 )
 
-func calculateFish(maxDays int) int {
-	var lanternFishList []int
-	day := 0
-	i := 0
-	fileInput, err := ioutil.ReadFile("day-6-input.txt")
-	strData := strings.Split(string(fileInput), ",")
-	for _, initTimerStr := range strData {
-		initTimerInt, err := strconv.Atoi(initTimerStr)
-		if err != nil {
-			log.Fatalf("error: cannot convert string to int %v\n", err)
-		}
-		lanternFishList = append(lanternFishList, initTimerInt)
-	}
+const (
+	ResetTimer = 6
+	MaxTimer   = 8
+)
 
+func calculateFish(maxDays int) uint64 {
+	var lanternFish [MaxTimer + 1]uint64
+	day := 0
+	fileInput, err := ioutil.ReadFile("day-6-input.txt")
 	if err != nil {
 		log.Fatalf("error: cannot open file %v\n", fileInput)
 	}
 
-	for true {
-		if lanternFishList[i] == 0 {
-			lanternFishList[i] = 6
-			lanternFishList = append(lanternFishList, 9)
-		} else {
-			lanternFishList[i]--
+	strData := strings.Split(string(fileInput), ",")
+	for _, initTimerStr := range strData {
+		timer, err := strconv.Atoi(initTimerStr)
+		if err != nil {
+			log.Fatalf("error: cannot convert string to int %v\n", err)
 		}
-		i = (i + 1) % len(lanternFishList)
-		if i == 0 {
-			day += 1
-			if day == maxDays {
-				break
-			}
+
+		if timer < 0 || timer > MaxTimer {
+			log.Fatalf("error: invalid timer value %v\n", timer)
+		}
+
+		lanternFish[timer]++
+	}
+
+	for true {
+		spawn := lanternFish[0]
+		for i := 1; i <= MaxTimer; i++ {
+			lanternFish[i-1] = lanternFish[i]
+		}
+		lanternFish[MaxTimer] = spawn
+		lanternFish[ResetTimer] += spawn
+
+		day += 1
+		if day == maxDays {
+			break
 		}
 	}
-	return len(lanternFishList)
+
+	totalFish := uint64(0)
+	for i := range lanternFish {
+		totalFish += lanternFish[i]
+	}
+	return totalFish
 }
+
 func main() {
 	fmt.Println("part1", calculateFish(80))
+	fmt.Println("part1", calculateFish(256))
 }
