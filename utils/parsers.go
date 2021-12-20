5e618421ef65c9f4c4dc12a5414681714f7894a0
Mark F. Brown <mark.brown314@gmail.com>
2021-12-20 12:48:10 -0500

    ConvStrToIntList() takes regex rather than string

--- utils/parsers.go
+++ utils/parsers.go
@@ -1,13 +1,19 @@
 package utils
 
 import (
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-func ConvStrToIntList(numList string, delimter string) ([]int, error) {
+func ConvStrToIntList(numList string, regex string) ([]int, error) {
 	numArray := make([]int, 0, 64)
-	for _, numStr := range strings.Split(numList, delimter) {
+	re, err := regexp.Compile(regex)
+	if err != nil {
+		return nil, err
+	}
+	numList = strings.TrimSpace(numList)
+	for _, numStr := range re.Split(numList, -1) {
 		numInt, err := strconv.Atoi(numStr)
 		if err != nil {
 			return nil, err
