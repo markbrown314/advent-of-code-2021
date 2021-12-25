a557166ee95504798b64b068f2f0b1d18735b4be
Mark F. Brown <mark.brown314@gmail.com>
2021-12-24 22:14:16 -0500

    Working on Day 8 Part 2

--- day-8/day-8.go
+++ day-8/day-8.go
@@ -8,9 +8,47 @@ import (
 	"strings"
 )
 
+var segmentMap map[int]int
+var wireMap map[rune]int
+
+func init() {
+
+	segmentMap = make(map[int]int, 7)
+	/* number = 0bgfedcba
+	    aaaa
+	   b    c
+	   b    c
+	    dddd
+	   e    f
+	   e    f
+	    gggg
+	*/
+
+	// bits are offset by one due to 0 = map failure
+	segmentMap[0] = 0b11101110
+	segmentMap[1] = 0b01001000
+	segmentMap[2] = 0b10111010
+	segmentMap[3] = 0b11011010
+	segmentMap[4] = 0b01011100
+	segmentMap[5] = 0b11010110
+	segmentMap[6] = 0b11110110
+	segmentMap[7] = 0b01001010
+	segmentMap[8] = 0b11111110
+	segmentMap[9] = 0b11011110
+
+	wireMap = make(map[rune]int)
+	wireMap['a'] = 1
+	wireMap['b'] = 2
+	wireMap['c'] = 3
+	wireMap['d'] = 4
+	wireMap['e'] = 5
+	wireMap['f'] = 6
+	wireMap['g'] = 7
+}
+
 func main() {
 	uniqueNumberCount := 0
-	fileInput, err := ioutil.ReadFile("day-8-input.txt")
+	fileInput, err := ioutil.ReadFile("day-8-test.txt")
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
@@ -26,7 +64,6 @@ func main() {
 			continue
 		}
 		for _, outputStr := range strings.Fields(inputStr[i]) {
-			//fmt.Println(outputStr, len(outputStr))
 			switch len(outputStr) {
 			case 2:
 				fallthrough
